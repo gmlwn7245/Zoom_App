d86bd13e31e8269fc2d4600f229138e47a2c83d8
gmlwn7245 <gmlwn7245@naver.com>
2023-03-02 19:46:30 +0900

    Express 설정

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 WebSocket과 WebRTC를 이용한 줌 클론 만들기 
 강의 : https://nomadcoders.co/courses
 
+***
 
-### 사용 기술 (언어, 프레임워크 등)
+## 사용 기술 (언어, 프레임워크 등)
 - JavaScript
 - VSCode
 - Node.js
--- src/server.js
+++ src/server.js
@@ -2,7 +2,17 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// views에 있는 pug 템플릿 보여줌
+app.set("view engine","pug");
+app.set("views", __dirname + "/views");
+
+// root 경로로 들어가면 home을 보여줌 (-> ./views/home.pug)
+app.get("/", (req, res)=> res.render("home"));
+
+// public 경로로 들어가면 public 하위 경로의 JS 실행 (static으로 설정해야 보임)
+app.use("/public", express.static(__dirname + "/public"));
+
+const handleListe = () => console.log(`Listending on http://localhost:3000`);
 
 // Server Port Number
 app.listen(3000);
