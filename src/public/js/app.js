cc482dcaf94ff85dfe3d8d2ed12f1ee5c82158cf
gmlwn7245 <gmlwn7245@naver.com>
2023-03-10 00:14:12 +0900

    카메라 선택 및 전환

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,23 +4,66 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-// 참조 : https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
-async function getMedia(){
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+
+        /* 카메라 선택 옵션 생성 코드 */
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;     // 선택 후 카메라 출력 변경을 위함
+            option.innerText = camera.label;
+            if(currentCamera.labe === camera.label){
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        });
+        
+
+        console.log(cameras);
+    }catch(e){
+        console.log(e);
+    }
+}
+
+// 참조 : https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+async function getMedia(deviceId){
+    const initalConstraints = {
+        audio:true,
+        video: { 
+            facingMode : "user"     // 후면카메라 - facingMode : "environment"
+        }
+    }
+
+    const cameraConstraints = {
+        audio:true,
+        deviceId: {
+            exact : deviceId        // 특정 deviceId로 카메라 설정
+        }
+    }
     try {
         /* 스트림 사용 */
         myStream = await navigator.mediaDevices.getUserMedia({
-            audio:true,
-            video:true
+            audio : true,
+            video : deviceId ? cameraConstraints : initalConstraints
         });
+
+        if(!deviceId){
+            await getCameras();
+        }
 
         /* 화면에 비디오 화면 띄우기 */
         myFace.srcObject = myStream;
-        console.log(myStream);
+
+        console.log(myStream.getVideoTracks());
     } catch(e) {
         /* 오류 처리 */
         console.log(e);
@@ -30,6 +73,8 @@ async function getMedia(){
 getMedia();
 
 function handleMuteClick(){
+    /* 오디오 트랙 정보 getAudioTracks() - enable 필드로 활성화 여부 변경 */
+    myStream.getAudioTracks().forEach(track => (track.enabled = !track.enabled));
     if(!muted){
         muteBtn.innerText = "UnMute";
         muted = true;
@@ -40,6 +85,8 @@ function handleMuteClick(){
     console.log("click Mute BTN");
 }
 function handleCameraClick(){
+    /* 비디오 트랙 정보 getVideoTracks() - enable 필드로 활성화 여부 변경 */
+    myStream.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
     if(cameraOff){
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
@@ -50,6 +97,11 @@ function handleCameraClick(){
     console.log("click Camera BTN");
 }
 
+async function handleCameraChange(){
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
 
